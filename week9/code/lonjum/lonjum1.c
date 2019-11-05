a4663214b8f61ec894f11acd4dedb8b7cf3eeb14
wcxCC <chengxuan.wu@qq.com>
2019-11-04 23:14:37 -0800

    update week9

---
+++ week9/code/lonjum/lonjum1.c
@@ -0,0 +1,28 @@
+#include <setjmp.h>
+#include "my.h"
+static jmp_buf g_stack_env;
+static void func1(void);
+static void func2(void);
+
+int main(void)
+{
+	if(0==setjmp(g_stack_env)){
+		printf("Normal flow\n");
+		func1();
+	}else{
+		printf("Long jump flow\n");
+	}
+	return 0;
+}
+
+static void func1(void)
+{
+	printf("Enter func1\n");
+	func2();
+}
+static void func2(void)
+{
+	printf("Enter func2\n");
+	longjmp(g_stack_env,1);
+	printf("Leave func2\n");
+}
---
+++ week9/code/mem/read.txt
@@ -0,0 +1,1 @@
+valgrind --track-fds=yes --leak-check=full --undef-value-errors=yes ./memtest
