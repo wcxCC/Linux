051ad79d1b565b69b92b50c0a55d25db71cd3979
wcxCC <chengxuan.wu@qq.com>
2019-12-10 03:47:49 -0800

    add week14

---
+++ week14/code/Tuesday/my.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <errno.h>
+#include<signal.h>
+#include<string.h>
+#include<pthread.h>
