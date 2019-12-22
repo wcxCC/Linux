641f4892d32e6c39877486519fb04e1ba03fb1b0
wcxCC <chengxuan.wu@qq.com>
2019-12-21 16:46:37 -0800

    add week15

---
+++ week15/code/Tuesday/my.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#define LOOP 10000000
+#define NUM 4
+#define TN 100
+#define WRITER_FIRST
+
